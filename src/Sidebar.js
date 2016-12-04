791e63db72a575122d1e4e7ee9f22a2fc4d31872
Seth Bonnie <sethkbonnie@gmail.com>
2016-12-04 13:43:36 -0600

    Add menu toggle

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import logo from './logo.svg';
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expand: false
+    }
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  toggleMenu() {
+    const expand = !this.state.expand;
+    this.setState({expand});
+  }
+
   render() {
+    const {expand} = this.state;
     return (
-      <div className="App-sidebar">
+      <div className={classnames("App-sidebar", {
+        expanded: expand,
+        collapsed: !expand
+      })}>
         <div className="App-sidebar-controls">
+          <i className="fa fa-bars fa-2x App-sidebar-menu-icon"
+             onClick={this.toggleMenu}/>
         </div>
         <div className="App-sidebar-content">
           <img src={logo} className="App-logo" alt="logo" />
